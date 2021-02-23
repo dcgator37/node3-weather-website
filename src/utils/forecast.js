014e8ca72046e417252f996383fa4af024eecbfe
Brent Baker <dcgator8@hotmail.com>
2021-02-23 07:21:05 -0500

    Added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
     } else if (body.error) {
       callback('Unable to find location');
     } else {
-      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees. It feels like ' + body.current.feelslike + ' degrees.');
+      callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees. It feels like ' + body.current.feelslike + ' degrees. The humidity is ' + body.current.humidity);
     }
   });
 };
